99eae00059a54d9ca2e23e6feed450875edfb940
Thuat <nguyenvanthuat250204@gmail.com>
2025-12-22 16:11:34 +0700

    fix

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get("/", (request, response) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 });
